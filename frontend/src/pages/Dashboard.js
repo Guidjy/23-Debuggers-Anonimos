c64df09d6ad1e8e3fc1ddb2cc48b12ebcc4874e9
Vitória Camara <120670231+iamvitoria@users.noreply.github.com>
2025-06-13 21:40:23 -0300

    feat: create dashboard

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+// src/pages/Dashboard.js
+function Dashboard() {
+  return (
+    <div className="p-10 bg-white min-h-screen">
+      <h2 className="text-3xl font-bold text-black">
+        Teste: Texto visível?
+      </h2>
+    </div>
+  );
+}
+
+export default Dashboard;
